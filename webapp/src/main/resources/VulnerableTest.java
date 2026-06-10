2c200c79d680e9b0a34668246e4376ba47ccf536
shubh <shubhamtakalikar1@gmail.com>
2026-06-10 16:46:26 +0530

    adding sonarqube plugin  script

---
+++ webapp/src/main/resources/VulnerableTest.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class VulnerableTest {
+    public static void main(String[] args) {
+        String password = "Admin@123"; // Hardcoded credential
+        System.out.println(password);
+    }
+}
